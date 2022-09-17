0c80996fc16d976c70d3c57a19b2b70a419697eb
alperenkavukk <kavuk.67.76@gmail.com>
2022-09-17 15:50:55 +0300

    Initial Commit

---
+++ NetworkInterChangable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkInterChangable
+//
+//  Created by Alperen Kavuk on 17.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkInterChangable/NetworkInterChangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterChangableApp.swift
+//  NetworkInterChangable
+//
+//  Created by Alperen Kavuk on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterChangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
